c62b8e38d98cab44c0f1e2af14f457618cad7586
Jason Bentley <jbxfade@fullsail.edu>
2012-05-30 23:09:16 -0700

    create select field element

--- main.js
+++ main.js
@@ -12,9 +12,18 @@ window.addEventListener("DOMContentLoaded", function(){
 		return theElement;
 	}
 
+
+	//Creat select field element and populate with options
+	function makeCats() {
+		var formTag = document.getElementsByTagName("form"),
+			selectLi = $("select"),
+			makeSelect = document.createElement("select");
+	}
+
+	//Variable Defaults
 	var mealType = [ "--Select--", "Chicken", "Beef", "Pork", "Veggie"];
 
-
+	//Set Link and Submit Click Events
 	var displayLink = $("display");
 	displayLink.addEventListener("click", getData);
 	var clearLink = $("clear");
