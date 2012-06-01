31e75fba6fd0cbe856955b91a6f02349aa7094d0
Jason Bentley <jbxfade@fullsail.edu>
2012-06-01 10:17:11 -0700

    added Select Option with javascript

--- main.js
+++ main.js
@@ -13,15 +13,25 @@ window.addEventListener("DOMContentLoaded", function(){
 	}
 
 
-	//Creat select field element and populate with options
+	//Create select field element and populate with options
 	function makeCats() {
 		var formTag = document.getElementsByTagName("form"),
 			selectLi = $("select"),
 			makeSelect = document.createElement("select");
+			makeSelect.setAttribute("id", "groups");
+		for(var i=0, j=mealType.length; i<j; i++){
+			var makeOption = document.createElement("option");
+			var optText = mealType[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);
 	}
 
 	//Variable Defaults
 	var mealType = [ "--Select--", "Chicken", "Beef", "Pork", "Veggie"];
+	makeCats();
 
 	//Set Link and Submit Click Events
 	var displayLink = $("display");
